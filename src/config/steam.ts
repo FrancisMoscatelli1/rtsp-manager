5e609ba0b68674d84b806d8aa32a0f40acff134b
Francis <38113140+FrancisMoscatelli1@users.noreply.github.com>
2025-09-23 11:05:22 -0300

    refactor: simplify FFMPEG pipeline configuration by removing unnecessary parameters

--- src/config/steam.ts
+++ src/config/steam.ts
@@ -1,22 +1,14 @@
 import config from './config'
 
 export const FFMPEG_PIPELINE = (rtspUrl: string, rtmpUrl: string) => [
-    '-xerror', '-fflags', 'nobuffer',
+    '-xerror', '-fflags', 'nobuffer', // Terminar en error si hay un problema
     '-loglevel', 'error',      // Solo mostrar errores
     '-nostats',                // No mostrar estadísticas de progreso
-    '-rtsp_transport', 'tcp',
-    '-re',
-    '-i', rtspUrl,
-    '-c:v', 'libx264',
-    '-preset', 'veryfast',
-    '-tune', 'zerolatency',
-    '-b:v', '1500k',
-    '-c:a', 'aac',
-    '-b:a', '128k',
-    '-ar', '44100',
-    '-c', 'copy',
-    '-f', 'flv',
-    rtmpUrl
+    '-rtsp_transport', 'tcp',  // Usar TCP para RTSP
+    '-i', rtspUrl, // Input RTSP URL
+    '-c', 'copy', // Copiar códec sin recodificar
+    '-f', 'flv', // Formato de salida FLV
+    rtmpUrl // Output RTMP URL
 ]
 
 
